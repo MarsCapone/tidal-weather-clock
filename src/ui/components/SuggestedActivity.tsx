66a2c2868614608444da50703d69304d92360faf
Samson Ventura-Danziger <samson@danziger.uk>
2025-07-19 00:09:18 +0200

    unhide dialog box on large screens

--- src/ui/components/SuggestedActivity.tsx
+++ src/ui/components/SuggestedActivity.tsx
@@ -90,7 +90,7 @@ export default function SuggestedActivity({
   const hasCardActions =
     ff.alwaysShowActivityNextButton || activitySelections.length > 1
 
-  const dialogId = 'activity-explanation-dialog'
+  const dialogId = `activity-explanation-dialog`
   const openDialog = () => {
     const dialog = document.getElementById(dialogId) as HTMLDialogElement
     if (dialog) {
@@ -99,62 +99,66 @@ export default function SuggestedActivity({
   }
 
   return (
-    <div className={`card card-lg shadow-sm ${className || ''}`}>
-      <div className="card-body">
-        <div className="card-title">Suggested Activity</div>
+    <>
+      <div className={`card card-lg shadow-sm ${className || ''}`}>
+        <div className="card-body">
+          <div className="card-title">Suggested Activity</div>
 
-        <p className="text-2xl font-extrabold">
-          {activitySelection.activity.displayName}
-        </p>
-        <div className="flex flex-row text-sm font-mono text-base-content/50">
-          <p>
-            {format(activitySelection.interval.start, 'HH:mm')} -{' '}
-            {format(activitySelection.interval.end, 'HH:mm')}
+          <p className="text-2xl font-extrabold">
+            {activitySelection.activity.displayName}
           </p>
-        </div>
-        <div className="card-actions">
-          <div className="w-full">
-            <div className="md:flex flex-col hidden lg:hidden gap-y-2 items-center">
-              <div
-                className="btn btn-primary flex-grow"
-                onClick={() => openDialog()}
-              >
-                Explain
+          <div className="flex flex-row text-sm font-mono text-base-content/50">
+            <p>
+              {format(activitySelection.interval.start, 'HH:mm')} -{' '}
+              {format(activitySelection.interval.end, 'HH:mm')}
+            </p>
+          </div>
+          <div className="card-actions">
+            <div className="w-full">
+              <div className="md:flex flex-col hidden lg:hidden gap-y-2 items-center">
+                <div
+                  className="btn btn-primary flex-grow"
+                  onClick={() => openDialog()}
+                >
+                  Explain
+                </div>
+                {hasCardActions && (
+                  <div className="join">
+                    <NavSuggestionButton
+                      direction="prev"
+                      disabled={selectionIndex === 0}
+                      className="join-item"
+                    />
+                    <NavSuggestionButton
+                      direction="next"
+                      disabled={
+                        selectionIndex === activitySelections.length - 1
+                      }
+                      className="join-item"
+                    />
+                  </div>
+                )}
               </div>
-              {hasCardActions && (
-                <div className="join">
+              <div className="md:hidden flex lg:flex flex-row justify-between gap-x-2">
+                {hasCardActions && (
                   <NavSuggestionButton
                     direction="prev"
                     disabled={selectionIndex === 0}
-                    className="join-item"
                   />
+                )}
+                <div
+                  className="btn btn-primary flex-grow"
+                  onClick={() => openDialog()}
+                >
+                  Explain
+                </div>
+                {hasCardActions && (
                   <NavSuggestionButton
                     direction="next"
                     disabled={selectionIndex === activitySelections.length - 1}
-                    className="join-item"
                   />
-                </div>
-              )}
-            </div>
-            <div className="md:hidden flex lg:flex flex-row justify-between gap-x-2">
-              {hasCardActions && (
-                <NavSuggestionButton
-                  direction="prev"
-                  disabled={selectionIndex === 0}
-                />
-              )}
-              <div
-                className="btn btn-primary flex-grow"
-                onClick={() => openDialog()}
-              >
-                Explain
+                )}
               </div>
-              {hasCardActions && (
-                <NavSuggestionButton
-                  direction="next"
-                  disabled={selectionIndex === activitySelections.length - 1}
-                />
-              )}
             </div>
           </div>
         </div>
@@ -163,11 +167,11 @@ export default function SuggestedActivity({
         activitySelection={activitySelection}
         id={dialogId}
       />
-    </div>
+    </>
   )
 }
 
-function ActivityExplanationDialog({
+export function ActivityExplanationDialog({
   activitySelection,
   id,
 }: {
