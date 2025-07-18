062ad0d02c1d5ce4fb251635233fcc2090d615b2
Samson Ventura-Danziger <samson@danziger.uk>
2025-07-18 13:14:45 +0200

    use daisyui colorscheme toggle

--- src/ui/components/Colorscheme.tsx
+++ src/ui/components/Colorscheme.tsx
@@ -2,18 +2,17 @@ import { MoonIcon, SunIcon } from '@heroicons/react/24/solid'
 import React from 'react'
 
 export default function ColorschemeToggle() {
-  const [isDark, setIsDark] = React.useState(false)
-
-  function toggleDarkMode() {
-    const added = document.documentElement.classList.toggle('dark')
-    setIsDark(added)
-  }
-
-  const Icon = isDark ? SunIcon : MoonIcon
-
   return (
-    <div className="w-6 h-6 m-3">
-      <Icon className="size-6" onClick={toggleDarkMode} />
+    <div>
+      <label className="flex cursor-pointer gap-2">
+        <SunIcon height={20} />
+        <input
+          type="checkbox"
+          value="night"
+          className="toggle theme-controller"
+        />
+        <MoonIcon height={20} />
+      </label>
     </div>
   )
 }
