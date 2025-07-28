99547f153f7e8d2a02ad51850da2542a8cfb794a
Samson Ventura-Danziger <samson@danziger.uk>
2025-07-28 20:19:32 +0100

    delete static example activities

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,3 @@
-import { Activity } from '@/types/activity'
-
 type Constants = {
   LOCATION_COORDS: [number, number]
   LOCATION_NAME: string
@@ -20,63 +18,3 @@ const CONSTANTS: Constants = {
 }
 
 export default CONSTANTS
-
-type ExampleData = {
-  Activities: Activity[]
-}
-export const EXAMPLE_DATA: ExampleData = {
-  Activities: [
-    {
-      constraints: [
-        { type: 'wind' },
-        { maxCloudCover: 80, type: 'weather' },
-        { requiresDaylight: true, type: 'sun' },
-        { earliestHour: 8, latestHour: 18, type: 'time' },
-      ],
-      description: 'Recreational sailing',
-      id: 'sailing',
-      name: 'Sailing',
-      priority: 7,
-    },
-    {
-      constraints: [
-        { preferredStates: ['rising', 'high'], type: 'tide' },
-        {
-          directionTolerance: 45,
-          maxSpeed: 12,
-          preferredDirections: [270, 225],
-          type: 'wind',
-        },
-        { requiresDaylight: true, type: 'sun' },
-        { minTemperature: 12, type: 'weather' },
-      ],
-      description: 'Surf session',
-      id: 'surfing',
-      name: 'Surfing',
-      priority: 8,
-    },
-    {
-      constraints: [
-        { requiresDarkness: true, type: 'sun' },
-        { maxCloudCover: 30, type: 'weather' },
-        { maxSpeed: 8, type: 'wind' },
-        { earliestHour: 21, type: 'time' },
-      ],
-      description: 'Astronomical observation',
-      id: 'stargazing',
-      name: 'Stargazing',
-      priority: 5,
-    },
-    {
-      constraints: [
-        { preferredStates: ['rising', 'falling'], type: 'tide' },
-        { maxSpeed: 10, type: 'wind' },
-        { preferredHours: [6, 7, 18, 19, 20], type: 'time' },
-      ],
-      description: 'Shore fishing',
-      id: 'fishing',
-      name: 'Fishing',
-      priority: 6,
-    },
-  ],
-}
