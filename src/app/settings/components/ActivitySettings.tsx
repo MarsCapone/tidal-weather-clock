1dd90f5bb0f24e678d8b79f6e8bf5278e0b0cc37
Samson Ventura-Danziger <samson@danziger.uk>
2025-07-29 13:32:23 +0100

    add edit and delete buttons

--- src/app/settings/components/ActivitySettings.tsx
+++ src/app/settings/components/ActivitySettings.tsx
@@ -4,15 +4,30 @@ import { APP_CONFIG } from '@/config'
 import { useActivities } from '@/hooks/useApiRequest'
 import { Activity, Constraint } from '@/types/activity'
 import { PencilIcon, TrashIcon } from '@heroicons/react/24/solid'
+import { useEffect, useState } from 'react'
 
 export default function ActivitySettings() {
-  const activities = useActivities(APP_CONFIG.activityFetcher)
+  const initialActivities = useActivities(APP_CONFIG.activityFetcher)
+  const [activities, setActivities] = useState<Activity[]>([])
+  useEffect(() => {
+    setActivities(initialActivities)
+  }, [initialActivities])
+
+  const onDeleteActivity = (id: string) => {
+    setActivities(activities.filter((item) => item.id !== id))
+  }
 
   return (
     <div>
       <h1 className="text-2xl font-bold">Activity Settings</h1>
       {activities.map((activity) => (
-        <ActivityCard key={activity.id} activity={activity} />
+        <ActivityCard
+          key={activity.id}
+          activity={activity}
+          onDelete={() => {
+            onDeleteActivity(activity.id)
+          }}
+        />
       ))}
     </div>
   )
@@ -20,16 +35,29 @@ export default function ActivitySettings() {
 
 type ActivityCardProps = {
   activity: Activity
+  onDelete: () => void
 }
 
-function ActivityCard({ activity }: ActivityCardProps) {
+function ActivityCard({ activity, onDelete }: ActivityCardProps) {
+  const onEdit = () => {}
+
   return (
     <div className="card card-lg my-2 shadow-sm">
       <div className="card-body">
         <div className="card-title flex flex-row justify-between">
           <div className="flex-1">{activity.name}</div>
-          <PencilIcon className="h-4 w-4" />
-          <TrashIcon className="h-4 w-4" />
+          <button
+            className="btn btn-ghost rounded-field aspect-square p-1"
+            onClick={onEdit}
+          >
+            <PencilIcon className="h-4 w-4" />
+          </button>
+          <button
+            className="btn btn-ghost rounded-field aspect-square p-1"
+            onClick={onDelete}
+          >
+            <TrashIcon className="h-4 w-4" />
+          </button>
         </div>
         <div>{activity.description}</div>
         <div
@@ -48,8 +76,11 @@ function ActivityCard({ activity }: ActivityCardProps) {
               available daily data.
             </div>
             <div>
-              {activity.constraints.map((constraint) => (
-                <ActivityConstraint constraint={constraint} />
+              {activity.constraints.map((constraint, i) => (
+                <ActivityConstraint
+                  key={`${i}:${constraint.type}`}
+                  constraint={constraint}
+                />
               ))}
             </div>
           </div>
