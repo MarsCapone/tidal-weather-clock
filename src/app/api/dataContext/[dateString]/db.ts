901016b09933adef778159f8a3db50bfaeffa629
Samson Ventura-Danziger <6295392+MarsCapone@users.noreply.github.com>
2025-08-03 00:56:50 +0100

    Add calendar selection for jump to a specific date (#20)

--- src/app/api/dataContext/[dateString]/db.ts
+++ src/app/api/dataContext/[dateString]/db.ts
@@ -8,6 +8,7 @@ const sql = neon(process.env.DATABASE_URL!)
 export default {
   addDataContext,
   getDataContextForDate,
+  getDataContextRange,
 }
 
 export async function addDataContext(
@@ -63,5 +64,30 @@ export async function getDataContextForDate(
   return {
     dataContext,
     lastUpdated,
+  }
+}
+
+export async function getDataContextRange(
+  location: [number, number],
+): Promise<{ earliest: string; latest: string; all: string[] }> {
+  const [result] = await sql`
+    SELECT MIN(date)::text AS earliest, MAX(date)::text AS latest
+    FROM datacontext
+    WHERE latitude = ${location[0]} AND longitude = ${location[1]}
+  `
+
+  const dates = await sql`
+  SELECT date::text FROM datacontext
+  WHERE latitude = ${location[0]} AND longitude = ${location[1]}
+  ORDER BY date DESC`
+
+  if (!result) {
+    throw new Error('No data context found for the given location')
+  }
+
+  return {
+    earliest: result.earliest,
+    latest: result.latest,
+    all: dates.map((d) => d.date),
   }
 }
---
+++ src/app/api/dataContext/route.ts
@@ -0,0 +1,11 @@
+import db from '@/app/api/dataContext/[dateString]/db'
+import CONSTANTS from '@/constants'
+import { NextRequest } from 'next/server'
+
+export async function GET(request: NextRequest): Promise<Response> {
+  // get metadata about the data context API
+
+  const range = await db.getDataContextRange(CONSTANTS.LOCATION_COORDS)
+
+  return Response.json(range)
+}
