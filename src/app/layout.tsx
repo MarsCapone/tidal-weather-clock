3ed6b46b997e02ed8d14f05ae8b5a5dd9a4d3cbd
Samson Ventura-Danziger <samson@danziger.uk>
2025-08-03 04:07:30 +0100

    add context to determine if dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,41 +4,46 @@ import ColorschemeToggle from '@/components/Colorscheme'
 import CONSTANTS from '@/constants'
 import Link from 'next/link'
 import './globals.css'
+import { IsDarkContext } from '@/utils/contexts'
 import { LDProvider } from 'launchdarkly-react-client-sdk'
 import React from 'react'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const [isDarkMode, setIsDarkMode] = React.useState<boolean>(false)
+
   return (
     <Root>
-      <div>
-        <div className="navbar bg-base-100 shadow-sm">
-          <div className="flex-1">
-            <Link className="btn btn-ghost text-md md:text-xl" href="/">
-              {CONSTANTS.TITLE}
-            </Link>
+      <IsDarkContext value={isDarkMode}>
+        <div>
+          <div className="navbar bg-base-100 shadow-sm">
+            <div className="flex-1">
+              <Link className="btn btn-ghost text-md md:text-xl" href="/">
+                {CONSTANTS.TITLE}
+              </Link>
+            </div>
+            <div className="flex-none">
+              <ul className="menu menu-horizontal items-center gap-x-2 px-1">
+                <li>
+                  <Link className="btn btn-ghost rounded-sm" href="/about">
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link className="btn btn-ghost rounded-sm" href={'/settings'}>
+                    Settings
+                  </Link>
+                </li>
+                <li>
+                  <ColorschemeToggle setIsDarkMode={setIsDarkMode} />
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className="flex-none">
-            <ul className="menu menu-horizontal items-center gap-x-2 px-1">
-              <li>
-                <Link className="btn btn-ghost rounded-sm" href="/about">
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link className="btn btn-ghost rounded-sm" href={'/settings'}>
-                  Settings
-                </Link>
-              </li>
-              <li>
-                <ColorschemeToggle />
-              </li>
-            </ul>
+          <div className="mx-auto flex min-w-full flex-col justify-center gap-10 p-10 text-center md:min-w-0">
+            {children}
           </div>
         </div>
-        <div className="mx-auto flex min-w-full flex-col justify-center gap-10 p-10 text-center md:min-w-0">
-          {children}
-        </div>
-      </div>
+      </IsDarkContext>
     </Root>
   )
 }
