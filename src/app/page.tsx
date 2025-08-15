5002ed12eb883808ba2d23f8bda4a6b92cb2550a
Samson Ventura-Danziger <samson@danziger.uk>
2025-08-16 00:40:54 +0100

    use server components for all pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,9 @@
-'use client'
-
 import MainContent from '@/components/MainContent'
-import useDateString from '@/hooks/useDateString'
 import { dateOptions } from '@/utils/dates'
-import { differenceInMilliseconds, startOfToday } from 'date-fns'
+import { startOfToday } from 'date-fns'
 import React from 'react'
-import logger from '../utils/logger'
 
-const today = startOfToday(dateOptions)
-
-export default function Page() {
-  const { date, nextPath, prevPath } = useDateString(undefined)
-  logger.info('Page render', {
-    date,
-    diff: differenceInMilliseconds(today, date),
-    differs: today !== date,
-    nextPath,
-    prevPath,
-  })
-  return <MainContent date={date} nextPath={nextPath} prevPath={prevPath} />
+export default async function Page() {
+  const today = startOfToday(dateOptions)
+  return <MainContent date={today} nextPath={'/plus/1'} prevPath={null} />
 }
