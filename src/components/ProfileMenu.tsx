cf6b8ca4de4c133aed9455b2aeb2e62aff6fc79d
Samson Ventura-Danziger <samson@danziger.uk>
2025-08-16 02:40:41 +0100

    style the login and logout menu

--- src/components/ProfileMenu.tsx
+++ src/components/ProfileMenu.tsx
@@ -1,56 +1,75 @@
 import { auth0 } from '@/lib/auth0'
-import { LogInIcon, LogOutIcon, LucideProps } from 'lucide-react'
-import React, { ForwardRefExoticComponent, RefAttributes } from 'react'
+import { LogInIcon, LogOutIcon } from 'lucide-react'
+import React from 'react'
 
 export default async function ProfileMenu() {
   const session = await auth0.getSession()
 
   if (!session) {
     return (
-      <ProfileMenuWrapper Icon={LogInIcon}>
-        <a href="/auth/login?screen_hint=signup">
-          <button>Sign up</button>
-        </a>
-        <a href="/auth/login">
-          <button>Log in</button>
-        </a>
+      <ProfileMenuWrapper buttonContent={<LogInIcon />}>
+        <div className={'menu w-full gap-2'}>
+          <li className={'menu-item'}>
+            <a
+              href="/auth/login?screen_hint=signup"
+              className={'btn btn-soft rounded-field'}
+            >
+              <span>Sign up</span>
+            </a>
+          </li>
+          <li className={'menu-item'}>
+            <a href="/auth/login" className={'btn btn-soft rounded-field'}>
+              <span>Log in</span>
+            </a>
+          </li>
+        </div>
       </ProfileMenuWrapper>
     )
   }
   return (
-    <ProfileMenuWrapper Icon={LogOutIcon}>
-      <h1>Welcome, {session.user.name}!</h1>
-      <p>
-        <a href="/auth/logout">
-          <button>Log out</button>
-        </a>
-      </p>
+    <ProfileMenuWrapper
+      buttonContent={
+        <div className="avatar">
+          <div className="w-10 rounded-full">
+            <img
+              src={session.user.picture}
+              alt={`Avatar for ${session.user.name}`}
+            />
+          </div>
+        </div>
+      }
+    >
+      <h4 className="text-xs">Welcome</h4>
+      <h1 className="text-md pl-4 font-bold">{session.user.name}</h1>
+      <h4 className="pl-4 font-mono text-xs">{session.user.email}</h4>
+      <div className="menu w-full gap-2">
+        <li className="menu-item">
+          <a href="/auth/logout" className="btn btn-soft rounded-field">
+            <span>Sign out</span>
+            <LogOutIcon className={'w-4'} />
+          </a>
+        </li>
+      </div>
     </ProfileMenuWrapper>
   )
 }
 
 function ProfileMenuWrapper({
-  Icon,
+  buttonContent,
   children,
 }: {
-  Icon: ForwardRefExoticComponent<
-    Omit<LucideProps, 'ref'> & RefAttributes<SVGSVGElement>
-  >
+  buttonContent: React.ReactNode
   children: React.ReactNode
 }) {
   return (
     <div>
-      <div className={'dropdown dropdown-end'}>
+      <div className={'dropdown dropdown-end dropdown-hover'}>
+        <div tabIndex={0}>{buttonContent}</div>
         <div
           tabIndex={0}
-          role={'button'}
-          className={'btn btn-ghost rounded-field'}
-        >
-          <Icon />
-        </div>
-        <div
-          tabIndex={0}
-          className={'dropdown-content bg-base-100 card card-sm z-1 shadow-sm'}
+          className={
+            'dropdown-content bg-base-100 card card-sm z-1 w-72 shadow-sm'
+          }
         >
           <div className={'card-body'}>{children}</div>
         </div>
--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -1,3 +1,12 @@
 import { Auth0Client } from '@auth0/nextjs-auth0/server'
+import { secondsInDay } from 'date-fns/constants'
 
-export const auth0 = new Auth0Client()
+export const auth0 = new Auth0Client({
+  session: {
+    rolling: true,
+    // every 30 days you have to login again
+    absoluteDuration: secondsInDay * 30,
+    // if you don't access for more than 7 days, you get logged out
+    inactivityDuration: secondsInDay * 7,
+  },
+})
