0826cd85d3092d679b041c756242f83c92d39f0b
Samson Ventura-Danziger <samson@danziger.uk>
2025-08-19 11:07:38 +0100

    fix other route

--- src/app/api/activity/route.ts
+++ src/app/api/activity/route.ts
@@ -2,7 +2,6 @@ import logger from '@/app/api/pinoLogger'
 import { getActivitiesByUserId, putActivities } from '@/lib/db/helpers/activity'
 import { Activity } from '@/lib/types/activity'
 import { NextRequest } from 'next/server'
-import { auth0 } from '@/lib/auth0'
 
 export async function GET(request: NextRequest): Promise<Response> {
   const searchParams = request.nextUrl.searchParams || {}
@@ -25,22 +24,19 @@ export async function GET(request: NextRequest): Promise<Response> {
   )
 }
 
-export const PUT = auth0.withApiAuthRequired(
-  async (request: NextRequest): Promise<Response> => {
-    const data: { userId?: string; activities: Activity[] } =
-      await request.json()
-    if (!data.activities) {
-      return Response.json({ error: 'Invalid request body' }, { status: 400 })
-    }
+export const PUT = async (request: NextRequest): Promise<Response> => {
+  const data: { userId?: string; activities: Activity[] } = await request.json()
+  if (!data.activities) {
+    return Response.json({ error: 'Invalid request body' }, { status: 400 })
+  }
 
-    const userId = data.userId || 'demouser'
-    const activities = data.activities
+  const userId = data.userId || 'demouser'
+  const activities = data.activities
 
-    // upsert all the activities
-    await putActivities(activities, userId)
+  // upsert all the activities
+  await putActivities(activities, userId)
 
-    logger.debug('added all constraints')
+  logger.debug('added all constraints')
 
-    return new Response('', { status: 201 })
-  },
-)
+  return new Response('', { status: 201 })
+}
