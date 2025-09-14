fc717cc665d51aa0b176d11a29c8cd8b664cdb4c
Samson Ventura-Danziger <samson@danziger.uk>
2025-09-14 03:24:14 +0100

    add types for contexts

--- src/lib/utils/contexts.ts
+++ src/lib/utils/contexts.ts
@@ -1,17 +1,29 @@
 import { TZDate } from '@date-fns/tz'
 import { createContext } from 'react'
 
-export const DarkModeContext = createContext({
+type ContextSetter<T, Name extends string> = {
+  [K in Name]: T
+} & {
+  [K in Name as `set${Capitalize<K>}`]: (value: T) => void
+}
+
+export const DarkModeContext = createContext<
+  ContextSetter<boolean, 'isDarkMode'>
+>({
   isDarkMode: false,
   setIsDarkMode: () => {},
 })
 
-export const TimeZoneContext = createContext({
-  timeZone: 'Etc/UTC',
-  setTimeZone: () => {},
-})
+export const TimeZoneContext = createContext<ContextSetter<string, 'timeZone'>>(
+  {
+    timeZone: 'Etc/UTC',
+    setTimeZone: () => {},
+  },
+)
 
-export const DateContext = createContext({
+export const DateContext = createContext<
+  ContextSetter<TZDate, 'date'> & { isPast: boolean }
+>({
   date: new TZDate(),
   isPast: false,
   setDate: () => {},
