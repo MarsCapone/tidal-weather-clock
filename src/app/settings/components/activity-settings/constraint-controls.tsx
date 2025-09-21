e20d39be16f0906a6210ce9a7e5661bc77d193d0
Samson Ventura-Danziger <samson@danziger.uk>
2025-09-21 22:07:03 +0100

    scale tide type selector on mobile

--- src/app/settings/components/activity-settings/constraint-controls.tsx
+++ src/app/settings/components/activity-settings/constraint-controls.tsx
@@ -194,7 +194,10 @@ export function TideConstraintControls({
         title={'Time from tidal event'}
         className={'grid grid-cols-3 gap-x-4'}
       >
-        <NamedFormComponent title={'Tide Type'}>
+        <NamedFormComponent
+          title={'Tide Type'}
+          className={'flex flex-col sm:flex-row'}
+        >
           <label className={'label'}>
             Low
             <input
@@ -204,6 +207,8 @@ export function TideConstraintControls({
               disabled={disabled}
               {...register(getTarget('eventType'))}
             />
+          </label>
+          <label className={'label'}>
             <input
               type="radio"
               value={'high'}
